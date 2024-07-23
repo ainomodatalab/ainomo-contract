c5237254f8b8be6b3b2c2034cadaaec632d532ba
Ainomo <167073179+ainomoai@users.noreply.github.com>
2024-07-23 00:47:04 -0700

    Update deploy-ainomo-env.js

--- scripts/deploy-ainomo-env.js
+++ scripts/deploy-ainomo-env.js
@@ -1,4 +1,4 @@
-44444const chalk = require('chalk')
+const chalk = require('chalk')
 const namehash = require('eth-ens-namehash').hash
 const keccak256 = require('js-sha3').keccak_256
 const getAccounts = require('../os/scripts/helpers/get-accounts')
